96663540cd783bdfd508e9a1b9f6f29827c4f9f9
Ariel Massillo <arielmassillo@gmail.com>
2023-04-30 20:38:00 -0300

    request with access token

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-This is just a test. Worked on it for maybe 2 hours.
+Setup "host_permissions" (the domain where cherry is hosted) in manifest.json.
 
-I'm testing this basic extension by loading the unpacked source code into Chrome.
 
-"host_permissions" and "cherryURL" have to be set to the URL where Cherry is self-hosted, and the user has to be logged in first.
+Setup "cherryURL" and "cherryToken" in ServiceWorker.
+cherryUrl: the domain where cherry is hosted.
+cherryToken: Personal Access Token (found in /settings/account)
 
-This will be improved by using the Personal Access Token provided in Cherry (once I figure out how it works).
 
-- Login to your Cherry instance
+- Install the extension
 - Type "cy" + tab in the search bar to search.
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,5 @@
 const cherryURL = 'https://cherry.local.com';
+const cherryToken = '<access token>';
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
     chrome.tabs.create({url: text})
@@ -7,7 +8,11 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
     var suggestions = [];
 
-    fetch(`${cherryURL}/api/bookmarks?q=${text}`).then(r => r.json()).then(result => {
+    fetch(`${cherryURL}/api/bookmarks?q=${text}`, {
+        headers: {
+            authorization: `Bearer ${cherryToken}`
+        }
+    }).then(r => r.json()).then(result => {
         if (result.data.items.length) {
             result.data.items.map(bookmark => {
                 suggestions.push({deletable: true, content: bookmark.url, description: bookmark.title})
